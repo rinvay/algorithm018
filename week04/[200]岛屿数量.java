afebd5c270cddaf3bf55ef6f2219f63982dc3c19
RinvayT <rinvaytang@126.com>
2020-11-16 00:07:55 +0800

    第4周作业add

---
+++ week04/[200]岛屿数量.java
@@ -0,0 +1,81 @@
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。 
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。 
+//
+// 此外，你可以假设该网格的四条边均被水包围。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：grid = [
+//  ["1","1","1","1","0"],
+//  ["1","1","0","1","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","0","0","0"]
+//]
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：grid = [
+//  ["1","1","0","0","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","1","0","0"],
+//  ["0","0","0","1","1"]
+//]
+//输出：3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == grid.length 
+// n == grid[i].length 
+// 1 <= m, n <= 300 
+// grid[i][j] 的值为 '0' 或 '1' 
+// 
+// Related Topics 深度优先搜索 广度优先搜索 并查集 
+// 👍 853 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int num = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if(grid[i][j] == '1') {
+                    num++;
+                    //找到一个岛屿后，把相连的路径置为0
+                    dfs(i, j, grid);
+                }
+            }
+        }
+        return num;
+    }
+
+    private void dfs(int row, int col, char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        if (row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] == '0') {
+            return;
+        }
+        grid[row][col] = '0';
+        //把相连的路径置为0，同属一个岛屿
+        dfs(row - 1, col, grid);
+        dfs(row + 1, col, grid);
+        dfs(row, col - 1, grid);
+        dfs(row, col + 1, grid);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
